f6c8013cc32080e701e5efd99018ffe413d7fa4e
lixiang <815589987@qq.com>
2021-10-12 16:48:26 +0800

    self-attention etc

--- attention-modules/self_attention.py
+++ attention-modules/self_attention.py
@@ -1,81 +1,85 @@
-import numpy as np
+"""
+self attention ViT Net paper,
+Transformer block.
+"""
 import torch
 from torch import nn
-from torch.nn import init
 
 
-class ScaledDotProductAttention(nn.Module):
-    """
-    Scaled dot-product attention
-    """
+def autopad(k, p=None):  # kernel, padding
+    # Pad to 'same'
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
 
-    def __init__(self, d_model, d_k, d_v, h, dropout=.1):
-        '''
-        :param d_model: Output dimensionality of the model
-        :param d_k: Dimensionality of queries and keys
-        :param d_v: Dimensionality of values
-        :param h: Number of heads
-        '''
-        super(ScaledDotProductAttention, self).__init__()
-        self.fc_q = nn.Linear(d_model, h * d_k)
-        self.fc_k = nn.Linear(d_model, h * d_k)
-        self.fc_v = nn.Linear(d_model, h * d_v)
-        self.fc_o = nn.Linear(h * d_v, d_model)
-        self.dropout = nn.Dropout(dropout)
 
-        self.d_model = d_model
-        self.d_k = d_k
-        self.d_v = d_v
-        self.h = h
+class Conv(nn.Module):
+    # Standard convolution
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
 
-        self.init_weights()
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
 
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
 
-    def forward(self, queries, keys, values, attention_mask=None, attention_weights=None):
-        '''
-        Computes
-        :param queries: Queries (b_s, nq, d_model)
-        :param keys: Keys (b_s, nk, d_model)
-        :param values: Values (b_s, nk, d_model)
-        :param attention_mask: Mask over attention values (b_s, h, nq, nk). True indicates masking.
-        :param attention_weights: Multiplicative weights for attention values (b_s, h, nq, nk).
-        :return:
-        '''
-        b_s, nq = queries.shape[:2]
-        nk = keys.shape[1]
 
-        q = self.fc_q(queries).view(b_s, nq, self.h, self.d_k).permute(0, 2, 1, 3)  # (b_s, h, nq, d_k)
-        k = self.fc_k(keys).view(b_s, nk, self.h, self.d_k).permute(0, 2, 3, 1)  # (b_s, h, d_k, nk)
-        v = self.fc_v(values).view(b_s, nk, self.h, self.d_v).permute(0, 2, 1, 3)  # (b_s, h, nk, d_v)
+class TransformerLayer(nn.Module):
+    # Transformer layer https://arxiv.org/abs/2010.11929 (LayerNorm layers removed for better performance)
+    def __init__(self, c, num_heads):
+        super().__init__()
+        self.q = nn.Linear(c, c, bias=False)
+        self.k = nn.Linear(c, c, bias=False)
+        self.v = nn.Linear(c, c, bias=False)
+        self.ma = nn.MultiheadAttention(embed_dim=c, num_heads=num_heads)
+        self.fc1 = nn.Linear(c, c, bias=False)
+        self.fc2 = nn.Linear(c, c, bias=False)
 
-        att = torch.matmul(q, k) / np.sqrt(self.d_k)  # (b_s, h, nq, nk)
-        if attention_weights is not None:
-            att = att * attention_weights
-        if attention_mask is not None:
-            att = att.masked_fill(attention_mask, -np.inf)
-        att = torch.softmax(att, -1)
-        att = self.dropout(att)
+    def forward(self, x):
+        x = self.ma(self.q(x), self.k(x), self.v(x))[0] + x
+        x = self.fc2(self.fc1(x)) + x
+        return x
 
-        out = torch.matmul(att, v).permute(0, 2, 1, 3).contiguous().view(b_s, nq, self.h * self.d_v)  # (b_s, nq, h*d_v)
-        out = self.fc_o(out)  # (b_s, nq, d_model)
-        return out
+
+class TransformerBlock(nn.Module):
+    # Vision Transformer https://arxiv.org/abs/2010.11929
+    # ① input shape: B C1 H W
+    # ② input shape data operate by flatten, transpose
+    #  B C1 H W ==>B C1 H*W ==> H*W B C1
+    # H*W B C1 :
+    # nn.Linear() 输入的[batch_size, size]
+    #             输出的[batch_size, out-size]
+    # 最后一维进行全连接操作，“ C1 ” 代表了二维特征(B C1 H W)
+    # 不同 channels 之间的 “多头注意力”方式。
+    # ③ output shape: B C2 H W
+    def __init__(self, c1, c2, num_heads, num_layers):
+        super().__init__()
+        self.conv = None
+        if c1 != c2:
+            self.conv = Conv(c1, c2)
+        self.linear = nn.Linear(c2, c2)  # learnable position embedding
+        self.tr = nn.Sequential(*[TransformerLayer(c2, num_heads) for _ in range(num_layers)])
+        self.c2 = c2
+
+    def forward(self, x):
+        if self.conv is not None:
+            x = self.conv(x)
+        b, _, w, h = x.shape
+        p = x.flatten(2).unsqueeze(0).transpose(0, 3).squeeze(3)
+        return self.tr(p + self.linear(p)).unsqueeze(3).transpose(0, 3).reshape(b, self.c2, w, h)
 
 
 if __name__ == '__main__':
-    input = torch.randn(50, 49, 512)
-    sa = ScaledDotProductAttention(d_model=512, d_k=512, d_v=512, h=8)
-    output = sa(input, input, input)
-    print(output.shape)
+    x = torch.randn(10, 32, 64, 64)
+    b, _, w, h = x.shape
+    p = x.flatten(2)
+    print(p.shape)
+    p = x.flatten(2).unsqueeze(0).transpose(0, 3).squeeze(3)
+    print(p.shape)
+    # sa = TransformerBlock(32, 16, 4, 2)
+    # output = sa(x)
+    # print(output.shape)
--- main.py
+++ main.py
@@ -19,8 +19,42 @@ def conv1x1(in_planes, out_planes, stride=1):
                      stride=stride, bias=False)
 
 
+class SeparableConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, dilation=1, bias=False,
+                 BatchNorm=nn.BatchNorm2d):
+        super(SeparableConv2d, self).__init__()
+
+        if dilation > kernel_size // 2:
+            padding = dilation
+        else:
+            padding = kernel_size // 2
+
+        self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding=padding,
+                               dilation=dilation, groups=in_channels, bias=bias)
+        # 带有groups的形式。
+        self.bn = nn.BatchNorm2d(in_channels)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, 1, 1, bias=bias)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn(x)
+        x = self.pointwise(x)
+        return x
+
+
+class BottleneckWithSeparableConv2d(nn.Module):
+    def __init__(self):
+        super(BottleneckWithSeparableConv2d, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
+
+
 if __name__ == '__main__':
-    data = torch.rand(10, 64, 521, 512)
-    se = CBAM_Attention(64)
-    out = se(data)
-    print(out.shape)
+    # data = torch.rand(10, 64, 521, 512)
+    # se = SeparableConv2d(64, 32, kernel_size=5, dilation=3)
+    # out = se(data)
+    # print(out.shape)
+    for i in range(16):
+        exec(f'print(block{i + 4})')
