a2c73baa96cf72431ce809ba43b472d361b1a05b
li.xiang <815589987@qq.com>
2021-10-19 10:08:41 +0800

    self-attention 模块添加了自注意力实现方法。

--- attention-modules/self_attention.py
+++ attention-modules/self_attention.py
@@ -1,10 +1,86 @@
 """
-self attention ViT Net paper, from yolov5.
-Transformer block.
-B C H W ==> to_qkv()
+（1） self-attention and multi-head attention
+    self-attention 实现 及 multi-head 实现。
+（2） self attention ViT Net paper, from yolov5.
+    Transformer block.
+    B C H W ==> to_qkv()
 """
 import torch
 from torch import nn
+# Muti-head Attention 机制的实现
+from math import sqrt
+
+
+# self-attention
+class Self_Attention(nn.Module):
+    """
+    # input shape : （batch_size * seq_len , input_dim）
+    # q : （batch_size * input_dim , dim_k）
+    # k : （batch_size * input_dim , dim_k）
+    # v : （batch_size * input_dim , dim_v）
+    """
+    def __init__(self, input_dim, dim_k, dim_v):
+        super(Self_Attention, self).__init__()
+        self.q = nn.Linear(input_dim, dim_k)
+        self.k = nn.Linear(input_dim, dim_k)
+        self.v = nn.Linear(input_dim, dim_v)
+        self._norm_fact = 1 / sqrt(dim_k)
+
+    def forward(self, x):
+        Q = self.q(x)  # Q: batch_size * seq_len * dim_k
+        K = self.k(x)  # K: batch_size * seq_len * dim_k
+        V = self.v(x)  # V: batch_size * seq_len * dim_v
+
+        atten = nn.Softmax(dim=-1)(
+            torch.bmm(Q, K.permute(0, 2, 1))) * self._norm_fact  # Q * K.T() # batch_size * seq_len * seq_len
+
+        output = torch.bmm(atten, V)  # Q * K.T() * V # batch_size * seq_len * dim_v
+
+        return output
+
+
+# multi-heads attention
+class Self_Attention_Muti_Head(nn.Module):
+    """
+    # input shape : （batch_size * seq_len , input_dim）
+    # q : （batch_size * input_dim , dim_k）
+    # k : （batch_size * input_dim , dim_k）
+    # v : （batch_size * input_dim , dim_v）
+    """
+    def __init__(self, input_dim, dim_k, dim_v, nums_head):
+        super(Self_Attention_Muti_Head, self).__init__()
+        assert dim_k % nums_head == 0
+        assert dim_v % nums_head == 0
+        self.q = nn.Linear(input_dim, dim_k)
+        self.k = nn.Linear(input_dim, dim_k)
+        self.v = nn.Linear(input_dim, dim_v)
+
+        self.nums_head = nums_head
+        self.dim_k = dim_k
+        self.dim_v = dim_v
+        self._norm_fact = 1 / sqrt(dim_k)
+
+    def forward(self, x):
+        # multi-head 的实现方法，即是 （self.dim_k // self.nums_head）
+        # 输出的q,k,v 切分成了多个 “批次”
+        Q = self.q(x).reshape(-1, x.shape[0], x.shape[1], self.dim_k // self.nums_head)
+        K = self.k(x).reshape(-1, x.shape[0], x.shape[1], self.dim_k // self.nums_head)
+        V = self.v(x).reshape(-1, x.shape[0], x.shape[1], self.dim_v // self.nums_head)
+        print(x.shape)
+        print(Q.size())
+
+        atten = nn.Softmax(dim=-1)(
+            torch.matmul(Q, K.permute(0, 1, 3, 2)))  # Q * K.T() # batch_size * seq_len * seq_len
+
+        output = torch.matmul(atten, V).reshape(x.shape[0], x.shape[1],
+                                                -1)  # Q * K.T() * V # batch_size * seq_len * dim_v
+
+        return output
+
+
+""" 
+以下代码来源于yolov5 用于处理卷积块 B C H W 为输入数据，输出为B C H W 的注意力机制实现。
+"""
 
 
 def autopad(k, p=None):  # kernel, padding
@@ -63,6 +139,7 @@ class TransformerBlock(nn.Module):
         if c1 != c2:
             self.conv = Conv(c1, c2)
         self.linear = nn.Linear(c2, c2)  # learnable position embedding
+        # multi heads , and multi layers
         self.tr = nn.Sequential(*[TransformerLayer(c2, num_heads) for _ in range(num_layers)])
         self.c2 = c2
 
